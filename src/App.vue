26dcc9c758fc0335a180e2368e8ce13b558f5150
Chhoungseang <chhoungseangung@gmail.com>
2024-10-24 11:18:09 +0700

    TP1: finish step 3

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>This is my first VueJs Project</div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
