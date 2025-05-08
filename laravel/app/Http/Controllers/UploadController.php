a35fcd6ad25f51b3a02622702359e97bea3136da
Chhoungseang <chhoungseangung@gmail.com>
2025-05-08 21:48:42 +0700

    Finished TP9

---
+++ laravel/app/Http/Controllers/UploadController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class UploadController extends Controller
+{
+    public function upload(Request $request)
+    {
+        // // Validate the request
+        // $request->validate([
+        // 'document' => 'required|file|mimes:jpg,jpeg,png,pdf|max:2048',
+        // ]);
+        // // Store the file
+        // $path = $request->file('document')->store('uploads', 'public');
+        // // Return a response
+        // return response()->json(['path' => $path], 200);
+        $this -> store($request);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'document' => 'required|image|max:2048' // Validation rules for upload 
+        ]);
+
+        $image = $request->file('document');
+        $fileName = uniqid() . '.' . $image->getClientOriginalExtension();
+
+        $path = $image->storeAs('uploads', $fileName);
+
+        $thumbnailPath = 'thumbnails/' . $fileName;
+        $intervention = Image::make($image->getRealPath());
+        $intervention->fit(200, 200, function ($constraint) {
+        $constraint->aspectRatio();
+        })->save(storage_path('app/public/' . $thumbnailPath));
+
+        return response()->json(['path' => $path], 200);
+    }
+}
--- laravel/config/filesystems.php
+++ laravel/config/filesystems.php
@@ -60,6 +60,18 @@ return [
             'report' => false,
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            // 'host' => env('MINIO_HOST', '127.0.0.1'),
+            'key' => env('MINIO_ACCESS_KEY_ID'),
+            'secret' => env('MINIO_SECRET_ACCESS_KEY'),
+            'region' => env('MINIO_REGION', 'us-east-1'),
+            'bucket' => env('MINIO_BUCKET'),
+            'endpoint' => env('MINIO_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'throw' => true,
+        ],
+
     ],
 
     /*
